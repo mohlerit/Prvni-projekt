6d4317e63c1bf57c6f6c4cbf3f30bebf686f8a5b
Michaela Šimková <simkova.misa@seznam.cz>
2022-05-02 17:29:39 +0200

    prvni commit

---
+++ .eleventy.js
@@ -0,0 +1,21 @@
+module.exports = function(eleventyConfig) {
+
+	// Výchozí výstupní složka: _site
+
+	// Zkopírovat images/ do _site/images
+	eleventyConfig.addPassthroughCopy("image");
+
+	// Zkopírovat css/ to _site/css/
+	eleventyConfig.addPassthroughCopy("style.css");
+
+  return {
+    // možné formáty šablon
+    templateFormats: ["njk", "html", "md", "liquid"],
+
+    // jako šablonovací jazyk zvolíme Nunjucks
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+  }
+
+};
